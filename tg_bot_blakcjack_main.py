ca2f932596edcac68a9e9fe59de4fa631b719b27
T1M3R0FF <118768348+T1M3R0FF@users.noreply.github.com>
2023-01-12 12:44:57 +0300

    fixed some checks, basically works

--- tg_bot_blakcjack_main.py
+++ tg_bot_blakcjack_main.py
@@ -1,7 +1,6 @@
 import telebot
 import config
 import random
-import time
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -13,7 +12,8 @@ cards = {'Шесть': 6, 'Семь': 7, 'Восемь': 8,
 data = list(cards.items())
 suits = ['пики', 'крести', 'черви', 'буби']
 counter = {}  # база данных игроков
-check_list = []
+check_set = set()
+
 
 @bot.message_handler(commands=['start'])  # старт игры
 def start(message):
@@ -22,61 +22,62 @@ def start(message):
         return True
     counter.update({message.from_user.id: 0})
     if len(counter) == 1:
-        bot.send_message(message.chat.id, 'Подожди, пока зарегистрируется второй игрок')
+        bot.send_message(message.chat.id, 'Приветствую, ты зарегистрировался на игру!\n'
+                                          'Подожди, пока зарегистрируется второй игрок')
     elif len(counter) == 2:
         for key in counter:
-            bot.send_message(key, 'Добро пожаловать в игру BlackJack! Да начнется игра!\n'
+            bot.send_message(key, 'Оба игрока на месте! Добро пожаловать в игру BlackJack! Да начнется игра!\n'
                                   'Возьмите себе карту через пункт меню "card"')
 
 
 @bot.message_handler(commands=['card'])  # взятие карты и зачисление очков
 def card(message):
     global counter
-
-    if len(counter) == 1:
-        bot.send_message(message.chat.id, 'Подожди, пока зарегистрируется второй игрок')
+    # проверки на дурака
+    if len(counter) == 0:
+        bot.send_message(message.chat.id, 'Для начала зарегистрируйся в игре через /start')
+    elif len(counter) == 1:
+        bot.send_message(message.chat.id, 'Приветствую, ты зарегистрировался на игру!\n'
+                                          'Подожди, пока зарегистрируется второй игрок')
 
     if counter[message.from_user.id] > 21:
-        bot.send_message(message.chat.id, f'Вы проиграли! Сумма ваших очков: {counter[message.from_user.id]}.'
-                                          'Нажмите /finish, чтобы узнать, кто победил')
-        counter[message.from_user.id] = 0
+        bot.send_message(message.chat.id, f'Для вас игра завершена, сумма ваших очков: {counter[message.from_user.id]}.'
+                                          'Нажмите /finish для завершения игры')
         return True
 
     key, value = random.choice(data)
     random_suit = random.choice(suits)
     counter[message.from_user.id] += value
+    bot.send_message(message.chat.id, f'Ваша карта : {key} {random_suit}, сумма очков: {counter[message.from_user.id]}')
+
     if counter[message.from_user.id] == 21:
         bot.send_message(message.chat.id, 'Блекджек! Вы набрали 21 очко, поздравляем!'
-                                          'Нажмите /finish, чтобы узнать, кто победил')
+                                          ' Нажмите /finish, чтобы узнать, кто победил')
         for key in counter:
             if key != message.from_user.id:
                 bot.send_message(key, 'У вашего соперника блекджек!')
         return True
 
     elif counter[message.from_user.id] > 21:
-        bot.send_message(message.chat.id, f'Игра окончена! Сумма ваших очков: {counter[message.from_user.id]}.\n'
+        bot.send_message(message.chat.id, f'Для вас игра завершена! Сумма ваших очков: {counter[message.from_user.id]}.'
                                           'Нажмите /finish, чтобы узнать, кто победил')
         return True
-
-    else:
-        bot.send_message(message.chat.id, f'Ваша карта : {key} {random_suit}\n'
-                                          'Если хотите завершить набор карт, нажмите /finish')
 
 
 @bot.message_handler(commands=['finish'])  # окончание игры
 def finish(message):
-    global check_list
+    global check_set
     global counter
 
     bot.send_message(message.chat.id, 'Вы решили закончить игру.')
     bot.send_message(message.chat.id, f'Сумма ваших очков: {counter[message.from_user.id]}.')
-    check_list.append(message.chat.id)
+    check_set.add(message.chat.id)
 
     # отправка очков сопернику
-    if len(check_list) == 1:
+    if len(check_set) == 1:
         bot.send_message(message.chat.id, 'Жду окончания игры твоего соперника')
 
-    elif len(check_list) == 2:
+    elif len(check_set) == 2:
         for key, key1 in counter, counter:
             if key != message.from_user.id and key1 == message.from_user.id:
                 bot.send_message(key, f'Соперник решил закончить набор карт, сумма его очков: {counter[key1]}')
@@ -100,8 +101,8 @@ def finish(message):
             bot.send_message(key, 'К сожалению,вы проиграли:(')
         bot.send_message(key, 'Если хотите сыграть еще, жмите /start')
         bot.send_message(key1, 'Если хотите сыграть еще, жмите /start')
-    counter.clear()
-    check_list = []
+        counter.clear()
+        check_set.clear()
     return True
 
 
